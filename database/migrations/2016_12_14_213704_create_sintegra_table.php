46cb56e78931a2a7d5e1d841b9221f4ce2c3b893
Mateus Padovani <mateuspadovani@MacBook-Pro-de-Mateus.local>
2016-12-15 15:48:41 -0200

    Alterada as config e criado migration

---
+++ database/migrations/2016_12_14_213704_create_sintegra_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSintegraTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('sintegra', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idusuario')->unsigned();
+            $table->foreign('idusuario')->references('id')->on('usuario');
+            $table->string('cnpj');
+            $table->text('resultado_json');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('sintegra');
+    }
+}
